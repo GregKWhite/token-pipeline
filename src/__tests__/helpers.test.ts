fc9ae6aa8ddab8f48dc794ea8822535a1a703e03
Greg White <gkwhite95@gmail.com>
2023-02-23 09:39:11 -0700

    Improve types and callbacks of pipe helpers

---
+++ src/__tests__/helpers.test.ts
@@ -0,0 +1,34 @@
+import {source} from '../pipe';
+import {mapValues, mapItems} from '../helpers';
+
+describe('helpers', () => {
+  describe('mapValues', () => {
+    it('returns a function that maps the values of an object', async () => {
+      const result = await source({a: 1, b: 2, c: 3})
+        .pipe(mapValues((num, key) => ({times2: num * 2, originalKey: key})))
+        .flow();
+
+      expect(result).toEqual({
+        a: {times2: 2, originalKey: 'a'},
+        b: {times2: 4, originalKey: 'b'},
+        c: {times2: 6, originalKey: 'c'},
+      });
+    });
+  });
+
+  describe('mapItems', () => {
+    it('returns a function that maps the items of an array', async () => {
+      const result = await source([1, 2, 3])
+        .pipe(
+          mapItems((num, index) => ({times2: num * 2, originalIndex: index}))
+        )
+        .flow();
+
+      expect(result).toEqual([
+        {times2: 2, originalIndex: 0},
+        {times2: 4, originalIndex: 1},
+        {times2: 6, originalIndex: 2},
+      ]);
+    });
+  });
+});
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,4 @@
-import {Union} from 'ts-toolbelt';
+import {Union, Misc} from 'ts-toolbelt';
 
 /**
  * A helper function for pipelines that returns a function that maps an array of items.
@@ -9,9 +9,33 @@ import {Union} from 'ts-toolbelt';
  * @param callback The function that maps each item in the array.
  * @returns A function that maps an array of items using the given callback.
  */
-export function mapItems<T, R>(callback: (arg: T) => R): (arg: T[]) => R[] {
-  return (data) => data.map(callback);
+export function mapItems<T, R>(
+  callback: (arg: T, index: number) => R
+): (arg: T[]) => R[] {
+  return (data) => data.map((item, index) => callback(item, index));
 }
+
+type MergedValues<T extends Record<any, any>> =
+  | PrimitiveValues<T>
+  | (Union.Merge<NonPrimitiveValues<T>> extends infer U
+      ? U[keyof U] extends never
+        ? never
+        : U
+      : never);
+
+type PrimitiveValues<T extends Record<any, any>> = Extract<
+  T[keyof T],
+  Misc.Primitive
+> extends any
+  ? Extract<T[keyof T], Misc.Primitive>
+  : never;
+
+type NonPrimitiveValues<T extends Record<any, any>> = Exclude<
+  T[keyof T],
+  Misc.Primitive
+> extends any
+  ? Exclude<T[keyof T], Misc.Primitive>
+  : never;
 
 /**
  * A helper function for pipelines that returns a function that maps the values of an object.
@@ -23,13 +47,13 @@ export function mapItems<T, R>(callback: (arg: T) => R): (arg: T[]) => R[] {
  * @returns A function that maps the values of an object using the given callback.
  */
 export function mapValues<T extends Record<any, any>, R>(
-  callback: (arg: Union.Merge<T[keyof T]>) => R
+  callback: (value: MergedValues<T>, key: keyof T) => R
 ) {
   return (data: T) => {
     return Object.keys(data).reduce((acc, key) => {
       return {
         ...acc,
-        [key]: callback(data[key]),
+        [key]: callback(data[key], key),
       };
     }, {} as {[key in keyof T]: R});
   };
